a6b6943ccd02dba53a114e3290c06117754d39e4
Nguyên Lê <nle372999@gmail.com>
2023-03-10 16:06:33 +0700

    ad(Layout): component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './routes/components/main/main.component';
+import { HomeComponent } from './routes/pages/home/home.component';
+import { TestComponent } from './routes/pages/test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'test', component: TestComponent },
+      { path: '', component: HomeComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './routes/components/header/header.component';
+import { FooterComponent } from './routes/components/footer/footer.component';
+import { MainComponent } from './routes/components/main/main.component';
+import { HomeComponent } from './routes/pages/home/home.component';
+import { TestComponent } from './routes/pages/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    HomeComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
